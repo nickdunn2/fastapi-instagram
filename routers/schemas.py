bd724fd6126295a5614094b6baa7d930e1d69bbc
Nick Dunn <nickdunn2@gmail.com>
2025-05-23 20:16:55 -0500

    feat(post): add schemas

--- routers/schemas.py
+++ routers/schemas.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel
 
 class UserBase(BaseModel):
@@ -8,6 +9,30 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
   username: str
   email: str
+
+  class Config():
+    orm_mode = True
+
+# For PostDisplay
+class User(BaseModel):
+  username: str
+
+  class Config():
+    orm_mode = True
+
+class PostBase(BaseModel):
+  image_url: str
+  image_url_type: str # relative or absolute
+  caption: str
+  creator_id: int
+
+class PostDisplay(BaseModel):
+  id: int
+  image_url: str
+  image_url_type: str # relative or absolute
+  caption: str
+  timestamp: datetime
+  user: User
 
   class Config():
     orm_mode = True
